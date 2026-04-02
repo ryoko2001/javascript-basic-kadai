821b556e139d602092494bdd485c18e1c077d987
ryoko2001 <ryokodesign01@gmail.com>
2026-04-02 11:16:10 +0900

    kadai_007提出

---
+++ kadai_007/calc.js
@@ -0,0 +1,22 @@
+// 変数numにランダムな正の数を代入
+let num = Math.floor(Math.random() * 50) + 1;
+
+// 変数numが3の倍数の場合「3の倍数です」と出力する
+if (num % 3 === 0) {
+  console.log('3の倍数です');
+}
+
+// 変数numが5の倍数の場合「5倍数です」と出力する
+else if (num % 5 === 0) {
+  console.log('5の倍数です');
+}
+
+// 変数numが3と5の倍数の場合「3と5の倍数です」と出力する
+else if (num % 3 === 0 && num % 5 === 0) {
+console.log('3と5の倍数です');
+}
+
+// それ以外の場合は変数numの値を出力する
+else {
+  console.log(num);
+}
